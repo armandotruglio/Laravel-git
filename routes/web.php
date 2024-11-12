b05e1162edc4ac115bd1cc103f6a2b9495b1fcbc
armandotruglio <armando.truglio1@gmail.com>
2024-11-12 12:57:57 +0100

    commit strano

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get("/posts/create", [PostController::class, "create"])->name("posts.create");
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::post("/posts", [PostController::class, "store"])->name("posts.store");
+Route::get("/posts/create", [PostController::class, "create"])->name("posts.create")->funzioneacaso();
